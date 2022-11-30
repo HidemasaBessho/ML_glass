121ebfe573074a93c27d5fe403bd4e47c637ce80
HidemasaBessho <96453382+HidemasaBessho@users.noreply.github.com>
2022-11-30 17:19:36 +0900

    Update jam.cpp

--- jam.cpp
+++ jam.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -40,14 +40,14 @@ double gaussian_rand(void)
   }
 }
 
-void ini_coord_rand(double (*x)[dim]){
+void ini_coord_rand(double (*x)[dim],double L){
   for(int i = 0 ; i < Np; i++){
     x[i][0] = L*unif_rand(0.,1.);
     x[i][1] = L*unif_rand(0.,1.);
-    }
+  }
 }
 
-set diameter(double *a,double r1,double r2){
+void set_diameter(double *a,double r1,double r2){
   int i,p=0;
   for(i=0;i<Np;i++){
     if(p==0){
@@ -59,10 +59,9 @@ set diameter(double *a,double r1,double r2){
       p = p-1;
     }
   }
-  return 0;
 }
 
-void p_boundary(double (*x)[dim]){
+void p_boundary(double (*x)[dim],double L){
   for(int i=0;i<Np;i++)
     for(int j=0;j<dim;j++)
       x[i][j]-=L*floor(x[i][j]/L);
@@ -87,62 +86,57 @@ int f(int i,int M)
   return k;
 }
 
-void cell_list(int (*list)[Nn],double (*x)[dim],int M,double Rcell)
-{
+void cell_list(int (*list)[Nn],double (*x)[dim],int M,double Rcell,double L){
   int i,j,k;
-  int nx,ny,nz;
-  int l,m,n;
-  double dx,dy,dz,r2;
-  
-  int (*map)[Np+1]=new int[M*M*M][Np+1];
-  
+  int nx,ny;
+  int l,m;
+  double dx,dy,r2;
+
+  int (*map)[Np]=new int[M*M][Np];
+
   for(i=0;i<M;i++)
     for(j=0;j<M;j++)
-      for(k=0;k<M;k++)
-	map[i+M*j+M*M*k][0]=0;
-  
+      map[i+M*j][0]=0;
+
   for(i=0;i<Np;i++){
     nx=f((int)(x[i][0]*M/L),M);
     ny=f((int)(x[i][1]*M/L),M);
-    nz=f((int)(x[i][2]*M/L),M);
-    for(n=nz-1;n<=nz+1;n++)
-      for(m=ny-1;m<=ny+1;m++)
-	for(l=nx-1;l<=nx+1;l++){
-	  map[f(l,M)+M*f(m,M)+M*M*f(n,M)][map[f(l,M)+M*f(m,M)+M*M*f(n,M)][0] +1]=i;
-	  map[f(l,M)+M*f(m,M)+M*M*f(n,M)][0]++;
-	}  
+
+    for(m=ny-1;m<=ny+1;m++){
+      for(l=nx-1;l<=nx+1;l++){
+        map[f(l,M)+M*f(m,M)][map[f(l,M)+M*f(m,M)][0] +1]=i;
+        map[f(l,M)+M*f(m,M)][0]++;
+      }
+    }
   }
-  
+
   for(i=0;i<Np;i++){
     list[i][0]=0;
     nx = f((int)(x[i][0]*M/L),M);
     ny = f((int)(x[i][1]*M/L),M);
-    nz = f((int)(x[i][2]*M/L),M);
-    
-    for (k=1; k<=(map[nx+M*ny+M*M*nz][0]); k++){
-      j = map[nx+M*ny+M*M*nz][k];
+
+    for (k=1; k<=(map[nx+M*ny][0]); k++){
+      j = map[nx+M*ny][k];
       if(j>i){
-	dx =x[i][0] - x[j][0];
-	dy =x[i][1] - x[j][1];
-	dz =x[i][2] - x[j][2];
+        dx =x[i][0] - x[j][0];
+        dy =x[i][1] - x[j][1];
 
-	dx-=L*floor((dx+0.5*L)/L);
-	dy-=L*floor((dy+0.5*L)/L);
-	dz-=L*floor((dz+0.5*L)/L);
+        dx-=L*floor((dx+0.5*L)/L);
+        dy-=L*floor((dy+0.5*L)/L);
 
-	r2 = dx*dx + dy*dy + dz*dz;
+        r2 = dx*dx + dy*dy;
 
-	if(r2<Rcell*Rcell){
-	  list[i][0]++;
-	  list[i][list[i][0]]=j;
-	}
+        if(r2<Rcell*Rcell){
+          list[i][0]++;
+          list[i][list[i][0]]=j;
+        }
       }
     }
   }
   delete []map;
 }
 
-void calc_force_hs(double (*x)[dim],double (*f)[dim],double *a,double *U,int (*list)[Nn]){
+void calc_force_hs(double (*x)[dim],double (*f)[dim],double *a,double *U,int (*list)[Nn],double L){
   double dx,dy,dr2,dr,dUr,aij,t;
   ini_array(f);
   *U=0;
@@ -150,7 +144,7 @@ void calc_force_hs(double (*x)[dim],double (*f)[dim],double *a,double *U,int (*l
     for(int j=1;j<=list[i][0];j++){
       dx=x[i][0]-x[list[i][j]][0];
       dy=x[i][1]-x[list[i][j]][1];
-      
+
       dx-=L*floor((dx+0.5*L)/L);
       dy-=L*floor((dy+0.5*L)/L);
       dr2=dx*dx+dy*dy;
@@ -168,17 +162,17 @@ void calc_force_hs(double (*x)[dim],double (*f)[dim],double *a,double *U,int (*l
   }
 }
 
-void eom_langevin_hs(double (*v)[dim],double (*x)[dim],double (*f)[dim],int *a,double *U,double dt,int (*list)[Nn],double *kine){
+void eom_langevin_hs(double (*v)[dim],double (*x)[dim],double (*f)[dim],double *a,double *U,double dt,int (*list)[Nn],double *kine,double L){
   double zeta=1.0;
   *kine=0.0;
-  calc_force_hs(x,f,a,&(*U),list);
+  calc_force_hs(x,f,a,&(*U),list,L);
   for(int i=0;i<Np;i++)
     for(int j=0;j<dim;j++){
       v[i][j]+=-zeta*v[i][j]*dt+f[i][j]*dt;
       x[i][j]+=v[i][j]*dt;
-      //*kine +=v[i][j]*v[i][j]/3.0/Np;
+      //*kine +=v[i][j]*v[i][j]/3.0/Np;                                                                                                                                                                                                                                         
     }
-  p_boundary(x);
+  p_boundary(x,L);
 }
 
 void update(double (*x_update)[dim],double (*x)[dim]){
@@ -187,9 +181,9 @@ void update(double (*x_update)[dim],double (*x)[dim]){
       x_update[i][j]=x[i][j];
 }
 
-void calc_disp_max(double *disp_max,double (*x)[dim],double (*x_update)[dim])
+void calc_disp_max(double *disp_max,double (*x)[dim],double (*x_update)[dim],double L)
 {
-  double dx,dy,dz;
+  double dx,dy;
   double disp;
   for(int i=0;i<Np;i++){
     dx=x[i][0]-x_update[i][0];
@@ -202,12 +196,12 @@ void calc_disp_max(double *disp_max,double (*x)[dim],double (*x_update)[dim])
   }
 }
 
-void auto_list_update(double *disp_max,double (*x)[dim],double (*x_update)[dim],int (*list)[Nn],int M,double Rcell){
+void auto_list_update(double *disp_max,double (*x)[dim],double (*x_update)[dim],int (*list)[Nn],int M,double Rcell,double L){
   static int count=0;
   count++;
-  calc_disp_max(&(*disp_max),x,x_update);
+  calc_disp_max(&(*disp_max),x,x_update,L);
   if(*disp_max > skin*skin*0.25){
-    cell_list(list,x,M,Rcell);
+    cell_list(list,x,M,Rcell,L);
     update(x_update,x);
     *disp_max=0.0;
     count=0;
@@ -227,10 +221,10 @@ void sizechange(double (*x)[dim],double *L,double d_phi,double alla,double *phi)
   *phi += d_phi;
 }
 
-void steepest_descent(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,double *U){
+void steepest_descent(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,double *U,double Rcell,double L){
   double dx,dy,dy_temp,dt0=0.0001,zeta=0.0,sum_force =0.0,dxmax=0.0,dymax=0.0,dummy=0.0;
   double x0[Np][dim],v[Np][dim];
-  cell_list(list,x,M,Rcell);
+  cell_list(list,x,M,Rcell,L);
 
   for(int i=0;i<Np;i++)
     for(int j=0;j<dim;j++){
@@ -239,7 +233,7 @@ void steepest_descent(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *
     }
 
   for(;;){
-    calc_force_hs(x,f,a,&(*U),list);
+    calc_force_hs(x,f,a,&(*U),list,L);
     sum_force=0.0;
     for(int i=0;i<Np;i++){
       for(int j=0;j<dim;j++){
@@ -252,16 +246,16 @@ void steepest_descent(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *
       dy -= L*floor((dy+0.5*L)/L);
       dx -= L*floor((dx+0.5*L)/L);
     }
-    
-    //    printf("SD: sum_force=%.16f,x=%f\n",sum_force,x[0][0]); 
-    
+
+    //    printf("SD: sum_force=%.16f,x=%f\n",sum_force,x[0][0]);                                                                                                                                                                                                               
+
     if(dx*dx+dy*dy > dxmax*dxmax+dymax*dymax){
       dxmax = dx;
       dymax = dy;
     }
     if(dxmax*dxmax+dymax*dymax > 0.5*skin*skin*0.25){
-      //printf("cell update: SD %f,%f,%f\n",dxmax,dymax,dzmax);
-      cell_list(list,x,M,Rcell);
+      //printf("cell update: SD %f,%f,%f\n",dxmax,dymax,dzmax);                                                                                                                                                                                                                 
+      cell_list(list,x,M,Rcell,L);
       for(int i=0;i<Np;i++){
         for(int j=0;j<dim;j++){
           x0[i][j]=x[i][j];
@@ -285,11 +279,11 @@ void norm_array(double *d,double (*x)[dim]){
   *d=0.0;
   for(int i=0;i<Np;i++)
     for(int j=0;j<dim;j++)
-    *d += x[i][j]*x[i][j];
+      *d += x[i][j]*x[i][j];
   *d = sqrt(*d);
 }
 
-int FIRE(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,double *U)
+int FIRE(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,double *U,double Rcell,double L)
 {
   int i,j,imax;
   double alpha = 0.1,P;
@@ -299,8 +293,8 @@ int FIRE(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,doubl
   int conv=0;
   double sum_force=0.0;
   double finc=1.1,falpha=0.99,fdec=0.5;
-  
-  cell_list(list,x,M,Rcell); 
+
+  cell_list(list,x,M,Rcell,L);
 
   for(i=0;i<Np;i++){
     for(j=0;j<dim;j++){
@@ -311,23 +305,9 @@ int FIRE(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,doubl
   P=0.0;
   dxmax = 0.0;
   dymax = 0.0;
-  
-  calc_force_hs(x,f,a,&(*U),list);
-   
-  for(;;){
-    P=0.0;
-    sum_force=0.0;
-    count++;
-    count0++;
-  
-    for(i=0;i<Np;i++){
-      for(j=0;j<dim;j++){
-        x[i][j]+=v[i][j]*dt0+0.5*f[i][j]*dt0*dt0;
-        v[i][j]+=0.5*f[i][j]*dt0;
-      }
-    }
-    calc_force_hs(x,f,a,&(*U),list);
-  
+
+  calc_force_hs(x,f,a,&(*U),list,L);
+
     for(i=0;i<Np;i++){
       for(j=0;j<dim;j++){
         v[i][j] += 0.5*f[i][j]*dt0;
@@ -335,44 +315,43 @@ int FIRE(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,doubl
     }
 
     norm_array(&f_nor,f);
-    norm_array(&v_nor,v);     
+    norm_array(&v_nor,v);
 
     for(i=0;i<Np;i++){
       for(j=0;j<dim;j++){
         v[i][j] = (1.0-alpha)*v[i][j]+alpha*f[i][j]/(f_nor+DBL_EPSILON)*v_nor;
       }
-    
-      P +=v[i][0]*f[i][0]+v[i][1]*f[i][1]+v[i][2]*f[i][2];      
+
+      P +=v[i][0]*f[i][0]+v[i][1]*f[i][1]+v[i][2]*f[i][2];
       sum_force += sqrt(f[i][0]*f[i][0]+f[i][1]*f[i][1]+f[i][2]*f[i][2])/Np;
-      
-      dy = x[i][1]-x0[i][1]; 
+
+      dy = x[i][1]-x0[i][1];
       dx = x[i][0]-x0[i][0];
       dy -= L*floor((dy+0.5*L)/L);
       dx -= L*floor((dx+0.5*L)/L);
-      
+
       if(dx*dx+dy*dy > dxmax*dxmax+dymax*dymax){
         dxmax = dx;
         dymax = dy;
         imax = i;
       }
     }
-    
+
     if(dxmax*dxmax+dymax*dymax > 0.5*skin*skin*0.25){
-      //printf("cell update: FIRE %f,%f,%f,imax=%d,U=%f\n",dxmax,dymax,dzmax,imax,*U/Np);
-      cell_list(list,x,M,Rcell);
-     
+      //printf("cell update: FIRE %f,%f,%f,imax=%d,U=%f\n",dxmax,dymax,dzmax,imax,*U/Np);                                                                                                                                                                                       
+      cell_list(list,x,M,Rcell,L);
+
       for(i=0;i<Np;i++){
         for(j=0;j<dim;j++){
           x0[i][j]=x[i][j];
         }
       }
-      
+
       dxmax=0.0;
       dymax=0.0;
-      dzmax=0.0;
     }
-    
-    //    printf("FIRE: sum_force=%.16f,dt=%f,x=%f, alpha=%f,P=%f,gamma=%f \n",sum_force,dt0,x[0][0],alpha,P,gamma);
+
+    //    printf("FIRE: sum_force=%.16f,dt=%f,x=%f, alpha=%f,P=%f,gamma=%f \n",sum_force,dt0,x[0][0],alpha,P,gamma);                                                                                                                                                            
     if(P>=0){
       conv++;
       if(conv>5){
@@ -383,21 +362,21 @@ int FIRE(double (*x)[dim],double (*f)[dim],int (*list)[Nn],double *a,int M,doubl
         conv=0;
       }
     }
-    
+
     if(P<0){
       alpha=0.1;
       dt0*=fdec;
       conv=0.0;
       for(i=0;i<Np;i++){
         for(j=0;j<dim;j++){
-          v[i][j]= 0.0; 
+          v[i][j]= 0.0;
         }
       }
     }
     if(sum_force <1.e-12||count>1e9){
-      p_boundary(double (*x)[dim]);
-      //std::cout<<"gamma="<<gamma<<"\t"<<"Iteration times ="<< count <<"\t"<<"energy="<<*U/(double)Np<<"\t"<<"stress ="<<*rfxy<<std::endl;
-      break;  
+      p_boundary(x,L);
+      //std::cout<<"gamma="<<gamma<<"\t"<<"Iteration times ="<< count <<"\t"<<"energy="<<*U/(double)Np<<"\t"<<"stress ="<<*rfxy<<std::endl;                                                                                                                                     
+      break;
     }
   }
   return 0;
@@ -425,33 +404,33 @@ int main()
   double RCHK=3.0;
   int M = (int)(L / RCHK);
   double Rcell = L/M;
-  
-  set_diameter(a);
-  ini_coord_rand(x);
+
+  set_diameter(a,r1,r2);
+  ini_coord_rand(x,L);
   ini_array(v);
-  cell_list(list,x,M,Rcell);
-  
+  cell_list(list,x,M,Rcell,L);
+
   for(t=0.0;t<100.0;t+=dt){
-    auto_list_update(&disp_max,x,x_update,list,M,Rcell);
-    eom_langevin_hs(v,x,f,a,&U,dtbdhs,0.0,list,&kine);
+    auto_list_update(&disp_max,x,x_update,list,M,Rcell,L);
+    eom_langevin_hs(v,x,f,a,&U,dt,list,&kine,L);
   }
-  
-  auto_list_update(&disp_max,x,x_update,list,M,Rcell);
-  
-  steepest_descent(x,f,list,*a,M,&U);
-  FIRE(x,f,list,a,M,&U);
-  
+
+  auto_list_update(&disp_max,x,x_update,list,M,Rcell,L);
+
+  steepest_descent(x,f,list,a,M,&U,Rcell,L);
+  FIRE(x,f,list,a,M,&U,Rcell,L);
+
   while(1){
     sizechange(x,&L,d_phi,alla,&phi);
     M = (int)(L / RCHK);
     Rcell = L/M;
-    steepest_descent(x,f,list,*a,M,&U);
-    FIRE(x,f,list,a,M,&U);
+    steepest_descent(x,f,list,a,M,&U,Rcell,L);
+    FIRE(x,f,list,a,M,&U,Rcell,L);
     output(phi,U);
     if(phi>0.86){
       break;
     }
   }
-  
+
   return 0;
 }
